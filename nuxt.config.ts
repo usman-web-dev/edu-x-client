bcbbddf6b56e39cca09d8d652523a6932a312833
Usman Javed <usman.javed@coeus-solutions.de>
2022-03-17 00:51:29 +0500

    EX-2 setup button component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,61 +1,66 @@
-import colors from 'vuetify/es5/util/colors'
+import { NuxtConfig } from '@nuxt/types';
 
-export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
+const config: NuxtConfig = {
   head: {
-    titleTemplate: '%s - edu-x-client',
-    title: 'edu-x-client',
+    titleTemplate: '%s - EduX',
+    title: 'EduX',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap'
+      }
+    ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+  components: [
+    { path: '@/components/', extensions: ['vue'] },
+    { path: '@/components/shared/', extensions: ['vue'] }
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify'],
+
   modules: [],
 
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
+    treeShake: true,
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
+        light: {
+          primary: '#4f46e5',
+          accent: '#111827',
+          secondary: '#1f2937',
+          warning: '#f97316',
+          error: '#dc2626',
+          success: '#16a34a'
+        }
+      }
     },
+    defaultAssets: {
+      font: false as unknown as undefined,
+      icons: 'mdi'
+    }
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
-}
+  build: {}
+};
+
+export default config;
