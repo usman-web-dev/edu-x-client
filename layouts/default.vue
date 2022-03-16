182132aaa88931dd80c1d96b0dd0ff954a56a372
Usman Javed <usman.javed@coeus-solutions.de>
2022-03-17 01:04:26 +0500

    EX-3 setup strapi

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,18 @@
   <v-app>
     <div class="d-flex justify-center flex-column align-center">
       <h1 class="text-h1">Oh haha! headings...</h1>
-      <base-btn>Oh haha!</base-btn>
+      <base-btn @click="login">Oh haha!</base-btn>
     </div>
   </v-app>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  methods: {
+    async login() {
+      await this.$strapi.login({ identifier: 'usman.dev03@gmail.com', password: 'Usman@0303' })
+    }
+  }
+})
+</script>
