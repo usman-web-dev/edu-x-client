72b048dbb4a89e7cbd55af632028715c52c9105d
Usman Dev <usman.developer002@gmail.com>
2022-03-17 23:48:16 +0500

    EX-7 add login page

--- components/base/card/index.vue
+++ components/base/card/index.vue
@@ -1,12 +1,12 @@
 <template>
   <v-card class="shadow">
-    <v-card-title v-if="$slots.title" class="pa-5 pb-0" :class="`justify-${titleIn}`">
+    <v-card-title v-if="$slots.title" class="pa-8 pb-0" :class="`justify-${titleIn}`">
       <slot name="title" />
     </v-card-title>
-    <v-card-text class="pa-5">
+    <v-card-text class="pa-8">
       <slot />
     </v-card-text>
-    <v-card-actions v-if="$slots.actions" class="light-gray px-5 py-3">
+    <v-card-actions v-if="$slots.actions" class="light-gray px-8 py-5">
       <slot name="actions" />
     </v-card-actions>
   </v-card>
