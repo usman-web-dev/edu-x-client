75b598c49e9a3807a994dc5ded0fbc5538cb02a2
Usman Dev <usman.developer002@gmail.com>
2022-03-20 11:07:29 +0500

    EX-2 remove vuex configs

--- store/base.module.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from '@nuxt/types';
-import { VuexModule } from 'nuxt-property-decorator';
-import { Api } from '~/plugins/api';
-
-export class BaseModule extends VuexModule {
-  $api!: Api;
-  $context!: Context;
-}
--- store/index.ts
+++
@@ -1,7 +0,0 @@
-import { Store } from 'vuex';
-
-const initStores = (store: Store<any>) => {};
-
-export const plugins = [initStores];
-
-export * from './base.module';
