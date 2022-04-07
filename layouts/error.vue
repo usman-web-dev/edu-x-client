ff7ae68dbacdcb28f38e6d6289257559c0b494ed
Usman Dev <usman.developer002@gmail.com>
2022-04-07 23:36:13 +0500

    EX-19 setup error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,39 +1,58 @@
 <template>
-  <v-app>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
-  </v-app>
+  <v-container class="fill-height secondary" fluid>
+    <v-row justify="center" align="center">
+      <v-col cols="12" lg="8" xl="6">
+        <div class="d-flex justify-center align-center align-lg-start flex-column flex-lg-row">
+          <h2 class="text-h4 text-lg-h2 primary--text font-weight-bold">{{ error.statusCode }}</h2>
+          <v-divider vertical class="grey darken-1 mx-8 d-none d-lg-block" />
+          <div class="text-center text-lg-left">
+            <h2 class="text-h4 text-lg-h2 white--text font-weight-bold">{{ errorDetails.title }}</h2>
+            <p class="text-body-2 grey--text mt-1 mt-lg-3 mb-0">{{ errorDetails.text }}</p>
+          </div>
+        </div>
+        <div class="text-center mt-10">
+          <base-btn :large="$vuetify.breakpoint.lgAndUp" to="/">
+            <v-icon color="white" class="mr-3">mdi-arrow-left</v-icon>
+            Go back home
+          </base-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'EmptyLayout',
-  layout: 'empty',
+  layout: 'guest',
   props: {
     error: {
       type: Object,
-      default: null,
-    },
+      default: null
+    }
   },
-  data() {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
+  computed: {
+    errorDetails() {
+      const { statusCode } = this.error;
+      if (statusCode === 404) {
+        return {
+          title: 'Page not found',
+          text: 'Please check the URL in the address bar and try again.'
+        };
+      }
+
+      return {
+        title: 'An error occurred',
+        text: 'Oops! something went wrong, please try again in a while.'
+      };
     }
   },
   head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    const title = this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
     return {
-      title,
-    }
-  },
-}
+      title
+    };
+  }
+};
 </script>
 
 <style scoped>
